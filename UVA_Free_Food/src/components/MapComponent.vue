1335ccc95db649857e0569af73e955b14b049f80
Gabi Turriago-Lopez <hsk3xb@virginia.edu>
2024-03-23 14:49:31 -0400

    idk

---
+++ UVA_Free_Food/src/components/MapComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="map" style="height: 400px;"></div>
+  </template>
+  
+  <script>
+  import L from 'leaflet';
+  
+  export default {
+    name: 'MapComponent',
+    mounted() {
+      this.initMap();
+    },
+    methods: {
+      initMap() {
+        const map = L.map('map').setView([51.505, -0.09], 13);
+  
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+      }
+    }
+  };
+  </script>
+  
